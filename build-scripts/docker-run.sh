272d8f90f2d3a5dd651683985de615574f814262
Matthew Little <zone117x@gmail.com>
2019-05-17 18:01:43 -0400

    Add circleCI config for dist build

--- build-scripts/docker-run.sh
+++ build-scripts/docker-run.sh
@@ -3,4 +3,13 @@
 script_path="$(dirname "$0")"
 src_dir="$(dirname "$script_path")"
 cd "$src_dir"
-docker run --volume `pwd`:/build --workdir /build --tty rust:1.34-stretch bash "$script_path/start-builds.sh"
+
+rust_image="rust:1.34-stretch"
+
+docker run \
+  --volume `pwd`:/build \
+  --workdir /build \
+  --tty \
+  --env "DIST_TARGET_FILTER=$DIST_TARGET_FILTER" \
+  $rust_image \
+  bash "$script_path/start-builds.sh"
